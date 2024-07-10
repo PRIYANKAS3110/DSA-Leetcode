eb6f729081798ec12532e2b5fb8459c498ed7402
priyankas3110 <priyankasekarjaya@gmail.com>
2024-07-10 09:26:20 +0530

    Crawler Log Folder

---
+++ Leetcode/Crawler Log Folder.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+class Solution {
+    public int minOperations(String[] logs) {
+        int c=0;
+        for(String log: logs){
+            if(log.equals("../")){
+               if(c>0){
+                c--;
+               }
+            }else if(!log.equals("./")){
+                c++;
+            }        
+        }
+        return c;
+    }
+}
