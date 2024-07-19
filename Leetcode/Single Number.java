b234fe7f46f841c637cf99025df2f5a7f35d2716
priyankas3110 <priyankasekarjaya@gmail.com>
2024-07-19 18:49:20 +0530

    136. Single Number

---
+++ Leetcode/Single Number.java
@@ -0,0 +1,11 @@
+package Leetcode;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for(int i=1;i<nums.length;i++){
+            result = result^nums[i];
+        }
+        return result;
+    }
+}
